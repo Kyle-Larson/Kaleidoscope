fb41f39e6a4df6f8e59fc42863064efe37c1155d
Kyle Larson <klarson1331@gmail.com>
2016-11-20 18:59:51 -0500

    Change to GLFW
    Changed to GLFW, builds on Emscripten and Native

--- EMSetup.bat
+++ EMSetup.bat
@@ -1,3 +1,3 @@
-emcmake cmake -H. -Bembuild
+cmake -DCMAKE_TOOLCHAIN_FILE="C:/Program Files/Emscripten/emscripten/1.35.0/cmake/Modules/Platform/Emscripten.cmake" -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ./ -H. -Bembuild
 cd embuild
 emmake make
--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,16 @@
 #include <stdio.h>
-#define SDL_MAIN_HANDLED
-#include <SDL.h>
+#define GLFW_INCLUDE_VLUKAN
+#include <GLFW/glfw3.h>
 
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 
-SDL_Window* window;
-SDL_Renderer* renderer;
-
-void loop() {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_Rect rect = { 0, 0, 100, 50 };
-    SDL_RenderFillRect(renderer, &rect);
-
-    SDL_RenderPresent(renderer);
-}
-
 int main() {
-    printf("hello, world!\n");
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("ERROR: %s\n", SDL_GetError());
-        return 1;
+    if (!glfwInit()) {
+        return -1;
     }
-
-    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
-    if (renderer == 0) {
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-#ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop_arg(loop, 60, 1);
-#else
-    while (true) {
-        loop();
-    }
-#endif
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    printf("Hello, world!");
+    glfwTerminate();
     return 0;
 }
