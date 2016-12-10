1a0a58b72d24b919ee1908a66f0295037a54fa2a
Kyle Larson <klarson1331@gmail.com>
2016-12-10 18:05:45 -0500

    Addition of GLEW

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
-cmake -H. -Bbuild
+cmake -H. -Bbuild -DCMAKE_GENERATOR_PLATFORM=x64
 cmake --build build --config Release
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,74 @@
 #include <stdio.h>
 
-#include <GLFW/glfw3.h>
-
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
 
+#define GLEW_STATIC
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+
 GLFWwindow* g_mainWindow;
+GLuint vertexBuffer, vertexShader, fragmentShader, program;
+GLint mvp_location, vpos_location, vcol_location;
+
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    { 0.6f, -0.4f, 0.f, 1.f, 0.f },
+    { 0.f, 0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"   color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"   gl_fragColor = vec4(color, 1.0);\n"
+"}\n";
+
+static void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void input_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 void loop(void) {
+    double time = glfwGetTime();
+    glfwPollEvents();
 
+
+    glfwSwapBuffers(g_mainWindow);
 }
 
 int main() {
+    glfwSetErrorCallback(error_callback);
+
     if (!glfwInit()) 
     {
         return -1;
     }
     
-    //OpenGL ES 2.0 Stuff
     glfwDefaultWindowHints();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
@@ -31,11 +78,26 @@ int main() {
         glfwTerminate();
         return -1;
     }
-
     glfwMakeContextCurrent(g_mainWindow);
 
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        return -1;
+    }
+
+    glfwSetKeyCallback(g_mainWindow, input_callback);
+    glfwMakeContextCurrent(g_mainWindow);
+    glfwSwapInterval(1);
+
+    int width, height;
+    glfwGetFramebufferSize(g_mainWindow, &width, &height);
+    glViewport(0, 0, width, height);
+
+
     #ifdef __EMSCRIPTEN__
-        emscripten_set_main_loop(loop, 60, 1);
+        emscripten_set_main_loop(loop, 0, 1);
     #else
         while(!glfwWindowShouldClose(g_mainWindow))
         {
